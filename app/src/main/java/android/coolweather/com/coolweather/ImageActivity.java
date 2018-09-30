09eb2e1be5aa4d336d39649e5ab1181ee312413c
Startyun <576903354@qq.com>
2018-09-30 11:42:23 +0800

    2018.9.30 12:00
    Material Design框架初步形成

--- app/src/main/java/android/coolweather/com/coolweather/ImageActivity.java
+++ app/src/main/java/android/coolweather/com/coolweather/ImageActivity.java
@@ -10,7 +10,10 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ImageActivity extends AppCompatActivity {
 
@@ -23,24 +26,48 @@ public class ImageActivity extends AppCompatActivity {
         Toolbar toolbar=(Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         mDrawerLayout=(DrawerLayout)findViewById(R.id.drawer_layout1);
-        //NavigationView navView=(NavigationView)findViewById(R.id.nav_view);
+        NavigationView navView=(NavigationView)findViewById(R.id.nav_view);
         ActionBar actionBar=getSupportActionBar();
         if(actionBar !=null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
 
         }
         mDrawerLayout.openDrawer(GravityCompat.START);
-//        navView.setCheckedItem(R.id.nav_call);
-//        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-//            @Override
-//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//                mDrawerLayout.closeDrawers();
-//                return true;
-//            }
-//        });
+       // navView.setCheckedItem(R.id.nav_call);
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.nav_call:
+                       Toast.makeText(ImageActivity.this, "点电话干嘛", Toast.LENGTH_SHORT).show();
+                       break;
+                    case R.id.nav_friends:
+                        Toast.makeText(ImageActivity.this, "点朋友干嘛", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_location:
+                        Toast.makeText(ImageActivity.this, "点位置干嘛", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_mail:
+                        Toast.makeText(ImageActivity.this, "点邮箱干嘛", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_task:
+                        Toast.makeText(ImageActivity.this, "点任务干嘛", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
+
+        View drawview = navView.inflateHeaderView(R.layout.nav_header);
+        CircleImageView icon_image = (CircleImageView) drawview.findViewById(R.id.icon_image);
+        icon_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ImageActivity.this,"我这么可爱，你点我干嘛！！！",Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
-
 
 
 
