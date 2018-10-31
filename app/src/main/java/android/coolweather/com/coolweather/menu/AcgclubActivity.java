8a501c39f8822bf701c44456c874f74d68b39767
Startyun <576903354@qq.com>
2018-10-31 17:53:11 +0800

    2018.10.30 18:00
    加入宅社接口

--- app/src/main/java/android/coolweather/com/coolweather/menu/AcgclubActivity.java
+++ app/src/main/java/android/coolweather/com/coolweather/menu/AcgclubActivity.java
@@ -1,16 +1,23 @@
 package android.coolweather.com.coolweather.menu;
 
+import android.content.SharedPreferences;
 import android.coolweather.com.coolweather.R;
+import android.coolweather.com.coolweather.WeatherActivity;
 import android.coolweather.com.coolweather.util.HttpUtil;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -21,14 +28,15 @@ import okhttp3.Response;
 public class AcgclubActivity extends AppCompatActivity {
 
     private TextView tet7;
+    private ImageView ima;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acgclub);
         tet7=(TextView)findViewById(R.id.tet7);
-        sendRequestWithOKHttp();
-
+        ima=(ImageView)findViewById(R.id.ima);
+        loadpicture();
     }
 
 
@@ -41,16 +49,30 @@ public class AcgclubActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call call, Response response) throws IOException {
                  String responseText =response.body().string();
+                 String title = null;
+               String image =null;
                  try {
                      JSONObject jsonObject = new JSONObject(responseText);
                      JSONArray jsonArray=jsonObject.getJSONArray("data");
                      JSONObject jsonObject1=jsonArray.getJSONObject(0);
-                     String title=jsonObject1.getString("title");
-                     Toast.makeText(AcgclubActivity.this, title, Toast.LENGTH_SHORT).show();
+                     title=jsonObject1.getString("title");
+                     JSONArray jsonArray1=jsonObject1.getJSONArray("imgUrls");
+                     image=jsonArray1.getString(0);
+                     //loadpicture1(image);
                  }catch (Exception e){
                      e.printStackTrace();
 
                  }
+                final String finalTitle = title;
+                 final String finalImage=image;
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        tet7.setText(finalTitle);
+                        Glide.with(AcgclubActivity.this).load(finalImage).into(ima);
+
+                    }
+                });
 
             }
             @Override
@@ -62,37 +84,6 @@ public class AcgclubActivity extends AppCompatActivity {
         });
     }
 
-
-
-    private void sendRequestWithOKHttp() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    OkHttpClient client = new OkHttpClient();
-                    Request request = new Request.Builder()
-                            .url("https://rabtman.com/api/v2/acgclub/pictures")
-                            .build();
-                    Response response = client.newCall(request).execute();
-                    String responseText = response.body().string();
-                    try {
-                        JSONObject jsonObject = new JSONObject(responseText);
-                        JSONArray jsonArray=jsonObject.getJSONArray("data");
-                        JSONObject jsonObject1=jsonArray.getJSONObject(0);
-                        String title=jsonObject1.getString("title");
-                        Toast.makeText(AcgclubActivity.this, title, Toast.LENGTH_SHORT).show();
-                    }catch (Exception e){
-                        e.printStackTrace();
-
-                    }
-
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }).start();
-    }
 
 
 
